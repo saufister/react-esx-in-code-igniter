e4ab6f137042e6208a98a881bf264cdffe666cc1
saufi <saufi@pdambandarmasih.com>
2024-01-31 13:23:03 +0800

    update

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -6,7 +6,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Your CodeIgniter App</title>
+    <title>React Jsx In Code Igniter</title>
 </head>
 <body>
 
